935ea1384735dac12feaf940f99220ad9db41709
Rodrigo Kamada <rodrigo@kamada.com.br>
2022-04-24 19:37:41 -0300

    Adding the environments settings.

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,22 @@
 export const environment = {
-  production: true
+  production: true,
+  amplify: {
+    Auth: {
+      identityPoolId: 'us-east-1:57a33aaf-0026-44e3-908c-7fb9d5730b9f',
+      region: 'us-east-1',
+    },
+    geo: {
+      AmazonLocationService: {
+        maps: {
+          items: {
+            AmazonMap: {
+              style: 'Default style',
+            }
+          },
+          default: 'AmazonMap',
+        },
+        region: 'us-east-1',
+      },
+    },
+  },
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,26 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  amplify: {
+    Auth: {
+      identityPoolId: 'us-east-1:57a33aaf-0026-44e3-908c-7fb9d5730b9f',
+      region: 'us-east-1',
+    },
+    geo: {
+      AmazonLocationService: {
+        maps: {
+          items: {
+            AmazonMap: {
+              style: 'Default style',
+            }
+          },
+          default: 'AmazonMap',
+        },
+        region: 'us-east-1',
+      },
+    },
+  },
 };
 
 /*
