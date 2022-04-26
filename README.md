62b4c38c16585695d6b95f15ae39cdcc4cb3ef43
Rodrigo Kamada <rodrigo@kamada.com.br>
2022-04-26 08:04:37 -0300

    Renaming the repository name.

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Angular Amazon Amplify Geo
+# Angular Map Amazon
 
 
 Application example built with [Angular](https://angular.io/) 13 and adding the map [MapLibre](https://maplibre.org/) component using the [Amazon Amplify Geo](https://docs.amplify.aws/lib/geo/getting-started/q/platform/js/) and [Amazon Location Service](https://aws.amazon.com/location/) services.
--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/angular-amazon-amplify-geo'),
+      dir: require('path').join(__dirname, './coverage/angular-map-amazon'),
       subdir: '.',
       reporters: [
         { type: 'html' },
